86c557b2af7795ae05a918d836828147961f6e11
mariociaponi <mario.lupo.ciaponi08@gmail.com>
2026-03-01 14:13:36 +0200

    feat: added navbar and hamburger menu components

---
+++ dolphinResidence/src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -0,0 +1,25 @@
+import "./HamburgerMenu.css";
+
+const HamburgerMenu = ({ showMenu }) => {
+  return (
+    <div className={`off-screen-menu ${showMenu ? "active" : ""}`}>
+      <ul className="links">
+        <li className="item">Начало</li>
+        <li data-target="about-us-section" className="item">
+          За нас
+        </li>
+        <li data-target="our-bungalows-section" className="item">
+          Бунгала
+        </li>
+        <li data-target="our-services-section" className="item">
+          Услуги
+        </li>
+        <li data-target="footer" className="item">
+          Контакти
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default HamburgerMenu;
---
+++ dolphinResidence/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faX } from "@fortawesome/free-solid-svg-icons";
+import HamburgerMenu from "../HamburgerMenu/HamburgerMenu";
+import Logo from "../../assets/logo.png";
+import "./Navbar.css";
+
+const Navbar = () => {
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
+
+  const toggleShowHamburgerMenu = () => setShowHamburgerMenu((prev) => !prev);
+
+  return (
+    <nav className="navbar">
+      <img className="logo" src={Logo} alt="logo" />
+
+      <ul className="links">
+        <li className="item">Начало</li>
+        <li data-target="about-us-section" className="item">
+          За нас
+        </li>
+        <li data-target="our-bungalows-section" className="item">
+          Бунгала
+        </li>
+        <li data-target="our-services-section" className="item">
+          Услуги
+        </li>
+        <li data-target="footer" className="item">
+          Контакти
+        </li>
+      </ul>
+
+      <HamburgerMenu showMenu={showHamburgerMenu} />
+
+      <button onClick={toggleShowHamburgerMenu} className="menu-button">
+        <FontAwesomeIcon icon={showHamburgerMenu ? faX : faBars} />
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
